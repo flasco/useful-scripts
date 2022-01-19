db5d160cc7e5c9be3a88612aeab409dd376c38f0
chenzhengqiang <chenzhengqiang@bytedance.com>
2022-01-19 16:08:58 +0800

    fix: 添加版本映射

--- src/n-switch.js
+++ src/n-switch.js
@@ -3,20 +3,54 @@ const path = require('path');
 const { $ } = require('zx');
 const semver = require('semver');
 
+const versionMapping = {
+  Argon: 4,
+  Boron: 6,
+  Carbon: 8,
+  Dubnium: 10,
+  Erbium: 12,
+  Fermium: 14,
+  Gallium: 16,
+};
+
+const getCurrentMajor = nvmVer => {
+  if (!nvmVer) {
+    return null;
+  }
+
+  if (nvmVer.includes('.')) {
+    return nvmVer.split('.')[0];
+  }
+
+  let major = null;
+  Object.keys(versionMapping).some(key => {
+    if (new RegExp(`${key}`, 'ig').test(nvmVer)) {
+      major = versionMapping[key];
+      return true;
+    }
+    return false;
+  });
+
+  return major;
+};
+
 const start = async () => {
   const nvmrcPath = path.join(process.cwd(), '.nvmrc');
   const nvmrcVer = fs.existsSync(nvmrcPath) ? fs.readFileSync(nvmrcPath, 'utf8') : '';
 
   if (nvmrcVer) {
-    // 如果指定了特殊的版本号也自动切换
-    if (nvmrcVer.includes('.')) {
-      const [targetMajor] = nvmrcVer.split('.');
+    const targetMajor = getCurrentMajor(nvmrcVer);
+    if (targetMajor !== null) {
       const [major] = process.versions.node.split('.');
-      if (major === targetMajor && semver.gte(nvmrcVer, process.versions.node)) {
-        console.log('n-switch: nvmrc is already set to the latest version.');
-        return;
+      if (major === targetMajor) {
+        // 如果是字母版本，就直接跳过，如果是数字版本，确认满足条件再跳过
+        if (!nvmrcVer.includes('.') || semver.gte(nvmrcVer, process.versions.node)) {
+          console.log('n-switch: nvmrc is already set to the latest version.');
+          return;
+        }
       }
     }
+
     // 只有major相同且 nvmrc 版本低于当前版本才不更新
     console.log(`n-switch: auto-switching, ${process.versions.node} -> ${nvmrcVer}`);
     await $`n auto`;
