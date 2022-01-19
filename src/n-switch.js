1b08125c9efabebe4bdc614efa62d389eca84947
chenzhengqiang <chenzhengqiang@bytedance.com>
2022-01-19 16:31:35 +0800

    fix: 优化提示

--- src/n-switch.js
+++ src/n-switch.js
@@ -38,25 +38,28 @@ const start = async () => {
   const nvmrcPath = path.join(process.cwd(), '.nvmrc');
   const nvmrcVer = fs.existsSync(nvmrcPath) ? fs.readFileSync(nvmrcPath, 'utf8') : '';
 
-  if (nvmrcVer) {
-    const targetMajor = getCurrentMajor(nvmrcVer);
-    if (targetMajor !== null) {
-      const [major] = process.versions.node.split('.');
-      if (Number(major) === Number(targetMajor)) {
-        // 如果是字母版本，就直接跳过，如果是数字版本，确认满足条件再跳过
-        if (!nvmrcVer.includes('.') || semver.gte(nvmrcVer, process.versions.node)) {
-          console.log(`n-switch: now use node v${process.versions.node}`);
-          return;
-        }
+  if (!nvmrcVer) {
+    console.log(`n-switch: now use node v${process.versions.node}`);
+    return;
+  }
+
+  const targetMajor = getCurrentMajor(nvmrcVer);
+  if (targetMajor !== null) {
+    const [major] = process.versions.node.split('.');
+    if (Number(major) === Number(targetMajor)) {
+      // 如果是字母版本，就直接跳过，如果是数字版本，确认满足条件再跳过
+      if (!nvmrcVer.includes('.') || semver.gte(nvmrcVer, process.versions.node)) {
+        console.log(`n-switch: now use node v${process.versions.node}`);
+        return;
       }
     }
-
-    // 只有major相同且 nvmrc 版本低于当前版本才不更新
-    console.log(`n-switch: auto-switching, ${process.versions.node} -> ${nvmrcVer}`);
-    spawn('n', ['auto'], {
-      stdio: 'inherit',
-    });
   }
+
+  // 只有major相同且 nvmrc 版本低于当前版本才不更新
+  console.log(`n-switch: auto-switching, ${process.versions.node} -> ${nvmrcVer}`);
+  spawn('n', ['auto'], {
+    stdio: 'inherit',
+  });
 };
 
 start();
